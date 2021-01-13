45bcbf9f9d637ab4d773dc0a47a287659e61f756
zhangrui654321 <58355935+zhangrui654321@users.noreply.github.com>
2021-01-13 16:23:13 +0800

    Update hizr.java

--- src/main/java/com/hizr/demo/hizr.java
+++ src/main/java/com/hizr/demo/hizr.java
@@ -2,6 +2,6 @@ package com.hizr.demo;
 
 public class hizr {
     public static void main(String args[]){
-        System.out.println("Hi World12346");
+        System.out.println("Hi World12346777");
     }
 }
