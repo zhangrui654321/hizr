f46a47ef7f01c3c8d73ad4e637de4e02582a7c5c
15172537049@163.com <zr525010>
2021-01-13 16:18:39 +0800

    test

--- src/main/java/com/hizr/demo/hizr.java
+++ src/main/java/com/hizr/demo/hizr.java
@@ -2,6 +2,6 @@ package com.hizr.demo;
 
 public class hizr {
     public static void main(String args[]){
-        System.out.println("Hi World123");
+        System.out.println("Hi World1234");
     }
 }
